238cd1948b552a3db0a600e84f1f48514b523ebd
Emanuel Peire <emapeire@outlook.com>
2023-08-15 17:17:58 -0300

    add next image

--- src/app/(banner)/posts/[id]/comments/page.jsx
+++ src/app/(banner)/posts/[id]/comments/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { LikeButton } from "../../LikeButton";
 
 const fetchComments = async (id) => {
@@ -30,12 +31,22 @@ export default async function Comments({ params }) {
       <ul>
         {comments.map((comment) => (
           <li key={comment.id}>
-            <div className="text-[#09f]">
-              <h3>{comment.name}</h3>
-            </div>
-            <p>{comment.body}</p>
-            <div className="flex justify-end my-10">
-              <LikeButton id={comment.id} />
+            <div className="flex gap-8 items-start">
+              <Image
+                width={100}
+                height={100}
+                alt={comment.name}
+                src={`https://api.dicebear.com/api/pixel-art-neutral/${comment.email}.svg`}
+              />
+              <div>
+                <div className="text-[#09f]">
+                  <h3>{comment.name}</h3>
+                </div>
+                <p>{comment.body}</p>
+                <div className="flex justify-end my-10">
+                  <LikeButton id={comment.id} />
+                </div>
+              </div>
             </div>
           </li>
         ))}
